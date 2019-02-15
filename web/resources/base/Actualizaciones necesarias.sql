faf3c3361e0c08abdd2aa0c1daa175c1f679df95
Erick <user@user-PC>
2019-02-15 12:08:30 -0600

    -Pasando a pantalla de mantenimiento. -Gestion de roles, ver "actualizaciones necesarias.sql"

--- web/resources/base/Actualizaciones necesarias.sql
+++ web/resources/base/Actualizaciones necesarias.sql
@@ -1,7 +1,8 @@
 --Fecha: 15/ febrero /2019
 --Error en en la entidad de paciente_usuario_usuario
 -- No existe la columna paciente_usuario_usuario
---Temporalmente agregada o cambiado la entidad. 
+--Temporalmente agregada esperar las modificaciones globales del sitio 
+alter table pacientes add COLUMN paciente_usuario_usuario CHARACTER VARYING(50);
 
 --Eliminando del rol Paciente los submenus:
 --Mis Citas Aprobadas  Id 6
@@ -11,4 +12,8 @@ delete from public.sub_menus_por_roles where rol_id = 5 and submenu_id=6;
 --INSERT INTO public.sub_menus_por_roles(rol_id, submenu_id) VALUES (5, 7);
 delete from public.sub_menus_por_roles where rol_id = 5 and submenu_id=7;
 
+--Paginas en mantenimiento
+--Gestion de roles. Manejo dinamico o estatico pendiente. 
+--submenu_id = 32
+update public.submenus set submenu_url = 'mantenimiento.xhtml' where submenu_id=32;
 

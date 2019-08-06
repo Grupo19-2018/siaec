8a07d0c71bcff48e841047e3a3975ee3ecd6eb83
erick <erick@Erick>
2019-08-06 15:03:27 -0600

    -Mejoras tablero estadisticas, medico.

--- web/resources/base/Actualizaciones necesarias.sql
+++ web/resources/base/Actualizaciones necesarias.sql
@@ -165,7 +165,7 @@ UPDATE usuarios SET usuario_contrasenia = '670b14728ad9902aecba32e22fa4f6bd';
 
 --Update 89
 --Agregar menu para bitacora
-INSERT INTO public.submenus (submenu_id, sumbenu_nombre, menu_id, submenu_descripcion, submenu_url) VALUES (1, 'Bitácora de Transacciones', 5, 'Clinica', '/views/5_reportes/rep_bitacora.xhtml');
+INSERT INTO public.submenus (submenu_id, sumbenu_nombre, menu_id, submenu_descripcion, submenu_url) VALUES (42, 'Bitácora de Transacciones', 5, 'Clinica', '/views/5_reportes/rep_bitacora.xhtml');
 --Asignar privilegios a rol admin y director para poder ver bitacora
 INSERT INTO public.sub_menus_por_roles (rol_id, submenu_id) VALUES (1, 42);
 INSERT INTO public.sub_menus_por_roles (rol_id, submenu_id) VALUES (2, 42);
